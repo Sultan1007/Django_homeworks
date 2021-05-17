b70e6faf9aaf7978678e35915124906e51b0931c
Justking 31 <kelsinbekovadilhan2018@gmail.com>
2021-05-17 18:08:02 +0600

    hw2

--- django_homeworks_3_curs/urls.py
+++ django_homeworks_3_curs/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from dz1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', views.get_all_products),
+    path('products/<int:id>/', views.get_one_product)
 ]
